12f43550e71fcf43363a576849c224a63a32af9d
Adam Aske <adamaske@live.no>
2025-04-07 02:30:24 +0200

    wrk

--- neuropipeline/neuropipeline/fnirs.py
+++ neuropipeline/neuropipeline/fnirs.py
@@ -116,10 +116,4 @@ class fNIRS(Base):
         
         
         pass
-snirf = fNIRS()
-snirf.read_snirf("C:/dev/neuro-glial-analysis/data/Subject01/Trial 3 - Supination/2025-03-24_003.snirf")
-snirf.feature_epochs(2, 2, 5)
-exit()
-snirf.wl_to_od()
-snirf.od_to_hb()
-snirf.write_snirf("C:/dev/test_snirf.snirf")
+    
