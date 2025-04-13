eb2b7ef868b3f4fbe66f64084e3dc75e81b77c6a
Adam Aske <adamaske@live.no>
2025-04-14 01:16:45 +0200

    fnirs wrapper work

--- neuropipeline/tests/tests.py
+++ neuropipeline/tests/tests.py
@@ -1,5 +1,7 @@
 from neuropipeline.fnirs import fNIRS
+from neuropipeline.eeg import EEG
 
+eeg = EEG()
 snirf = fNIRS()
 snirf.read_snirf("C:/dev/neuro-glial-analysis/data/Subject01/Trial 3 - Supination/2025-03-24_003.snirf")
 snirf.feature_epochs(2, 2, 5)
